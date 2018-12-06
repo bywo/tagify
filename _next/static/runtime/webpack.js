6e0461ae0fb25b7815b24991c95b5a6969eb7c67
Byron <byronmwong@gmail.com>
2018-12-05 20:29:27 -0800

    Updates

--- _next/static/runtime/webpack.js
+++ _next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "17113b52385bcc93e613";
+/******/ 	var hotCurrentHash = "24f19d3779811a4c3d56";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- _next/static/webpack/static/development/pages/index.js.dc5ccc63334df905165e.hot-update.js
+++ _next/static/webpack/static/development/pages/index.js.dc5ccc63334df905165e.hot-update.js
@@ -4,7 +4,7 @@ webpackHotUpdate("static/development/pages/index.js",{
 /*!*************************!*\
   !*** ./data/UIStore.js ***!
   \*************************/
-/*! exports provided: onResizeSidebar, sidebarWidth$, onResizeTrackCol, trackColWidth$, onResizeArtistCol, artistColWidth$, onSelectPlaylist, selectedPlaylist$, onChangeSearchQuery, searchQuery$ */
+/*! exports provided: onResizeSidebar, sidebarWidth$, onResizeTrackCol, trackColWidth$, onResizeArtistCol, artistColWidth$, onSelectPlaylist, selectedPlaylistChanges$, selectedPlaylist$, onChangeSearchQuery, searchQueryChanges$, searchQuery$ */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
@@ -16,19 +16,15 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "onResizeArtistCol", function() { return onResizeArtistCol; });
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "artistColWidth$", function() { return artistColWidth$; });
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "onSelectPlaylist", function() { return onSelectPlaylist; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "selectedPlaylistChanges$", function() { return selectedPlaylistChanges$; });
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "selectedPlaylist$", function() { return selectedPlaylist$; });
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "onChangeSearchQuery", function() { return onChangeSearchQuery; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "searchQueryChanges$", function() { return searchQueryChanges$; });
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "searchQuery$", function() { return searchQuery$; });
-/* harmony import */ var lodash__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! lodash */ "./node_modules/lodash/lodash.js");
-/* harmony import */ var lodash__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(lodash__WEBPACK_IMPORTED_MODULE_0__);
-/* harmony import */ var xstream__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! xstream */ "./node_modules/xstream/index.js");
-/* harmony import */ var xstream__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(xstream__WEBPACK_IMPORTED_MODULE_1__);
-/* harmony import */ var _util_recompose__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../util/recompose */ "./util/recompose.js");
+/* harmony import */ var _util_recompose__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../util/recompose */ "./util/recompose.js");
 
 
-
-
-var _createEventHandler = Object(_util_recompose__WEBPACK_IMPORTED_MODULE_2__["createEventHandler"])(),
+var _createEventHandler = Object(_util_recompose__WEBPACK_IMPORTED_MODULE_0__["createEventHandler"])(),
     onResizeSidebar = _createEventHandler.handler,
     sidebarDeltas$ = _createEventHandler.stream;
 
@@ -37,7 +33,7 @@ var sidebarWidth$ = sidebarDeltas$.fold(function (acc, x) {
   return acc + x;
 }, 200);
 
-var _createEventHandler2 = Object(_util_recompose__WEBPACK_IMPORTED_MODULE_2__["createEventHandler"])(),
+var _createEventHandler2 = Object(_util_recompose__WEBPACK_IMPORTED_MODULE_0__["createEventHandler"])(),
     onResizeTrackCol = _createEventHandler2.handler,
     trackColDeltas$ = _createEventHandler2.stream;
 
@@ -46,7 +42,7 @@ var trackColWidth$ = trackColDeltas$.fold(function (acc, x) {
   return acc + x;
 }, 200);
 
-var _createEventHandler3 = Object(_util_recompose__WEBPACK_IMPORTED_MODULE_2__["createEventHandler"])(),
+var _createEventHandler3 = Object(_util_recompose__WEBPACK_IMPORTED_MODULE_0__["createEventHandler"])(),
     onResizeArtistCol = _createEventHandler3.handler,
     artistColDeltas$ = _createEventHandler3.stream;
 
@@ -55,14 +51,14 @@ var artistColWidth$ = artistColDeltas$.fold(function (acc, x) {
   return acc + x;
 }, 200);
 
-var _createEventHandler4 = Object(_util_recompose__WEBPACK_IMPORTED_MODULE_2__["createEventHandler"])(),
+var _createEventHandler4 = Object(_util_recompose__WEBPACK_IMPORTED_MODULE_0__["createEventHandler"])(),
     onSelectPlaylist = _createEventHandler4.handler,
     selectedPlaylistChanges$ = _createEventHandler4.stream;
 
 
 var selectedPlaylist$ = selectedPlaylistChanges$.startWith("all");
 
-var _createEventHandler5 = Object(_util_recompose__WEBPACK_IMPORTED_MODULE_2__["createEventHandler"])(),
+var _createEventHandler5 = Object(_util_recompose__WEBPACK_IMPORTED_MODULE_0__["createEventHandler"])(),
     onChangeSearchQuery = _createEventHandler5.handler,
     searchQueryChanges$ = _createEventHandler5.stream;
 
@@ -72,4 +68,4 @@ var searchQuery$ = searchQueryChanges$.startWith("");
 /***/ })
 
 })
-//# sourceMappingURL=index.js.7c5957f5093b68445390.hot-update.js.map
+//# sourceMappingURL=index.js.dc5ccc63334df905165e.hot-update.js.map
